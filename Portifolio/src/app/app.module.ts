2f272df3d7cd90d924c943fee3693004d61c210a
julien <gorguetmoura@gmail.com>
2024-11-05 18:02:54 -0300

    Novo Package para mudanças de Idioma

--- Portifolio/src/app/app.module.ts
+++ Portifolio/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
+import { HttpLoaderFactory } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,15 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
