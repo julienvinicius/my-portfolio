4c7ccd59165c57a078748f8d86061ed00eb48b2f
julien <gorguetmoura@gmail.com>
2024-11-07 14:19:33 -0300

    Readme para futuras melhorias

--- Portifolio/src/app/app.module.ts
+++ Portifolio/src/app/app.module.ts
@@ -2,37 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from './app.component'; // Importar o AppComponent
+import { HeaderComponent } from './header/header.component'; // Importar o HeaderComponent
+import { AboutComponent } from './about/about.component'; // Importar o AboutComponent
+import { ProjectsComponent } from './projects/projects.component'; // Importar o ProjectsComponent
+import { ContactComponent } from './contact/contact.component'; // Importar o ContactComponent
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient } from '@angular/common/http';
-import { HttpLoaderFactory } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser'; // Importar bootstrapApplication
+
+// Defina a função HttpLoaderFactory corretamente
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    AboutComponent,
-    ProjectsComponent,
-    ContactComponent
+    // Remova os componentes autônomos da declaração
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
         deps: [HttpClient]
       }
-    })
-    
+    }),
+    HeaderComponent, // Importar o HeaderComponent aqui
+    AboutComponent, // Importar o AboutComponent aqui
+    ProjectsComponent, // Importar o ProjectsComponent aqui
+    ContactComponent // Importar o ContactComponent aqui
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// Use bootstrapApplication para inicializar a aplicação
+bootstrapApplication(AppComponent);
